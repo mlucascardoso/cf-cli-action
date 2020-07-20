f614fbd98483a6dd99d69e26a05991f326de27da
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-07-20 09:14:18 -0300

    chore: test new commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-# if [ $INPUT_USE_MULTI_APPS_PLUGIN = "true" ] 
-# then
+if [ $INPUT_USE_MULTI_APPS_PLUGIN = "true" ] 
+then
     sh -c "cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org"
     sh -c "cf install-plugin -r CF-Community 'multiapps' -f"
-# fi
+fi
 
 sh -c "cf $*"
